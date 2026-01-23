f2a5838b202cc015b762e6f50de8fc7fb27c1827
Hogyu,Kim <hgkimer@gmail.com>
2026-01-23 12:54:39 +0900

    feat(boot): 메인 어플리케이션 클래스 및 초기 스프링 설정 파일 커밋

---
+++ src/main/java/io/github/hgkimer/privateblog/PrivateBlogApplication.java
@@ -0,0 +1,13 @@
+package io.github.hgkimer.privateblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hgkimer/privateblog/PrivateBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hgkimer.privateblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
